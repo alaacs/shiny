d7e00f2b0d86feda91b469c43a942f2ede6125e9
alaacs <alaa.cs@hotmail.com>
2015-01-23 18:31:51 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,7 +8,10 @@ shinyUI(pageWithSidebar(
     sliderInput(inputId = "sliderRatioCnt", label = "Number of ratios", min = 1, max = 50, value = 10)
 		),
 	mainPanel(
-		showOutput("resultPlot", "polycharts")
+		showOutput("resultPlot", "polycharts"),
+    a("Click here for documentation", 
+      href="https://docs.google.com/document/d/1hIXPwUtXPVuhPIiJe1Yc1k-yguuc6swUE-4EihVuOY4/pub#ftnt1",
+      target="_blank")
 		)
 	)
 )
